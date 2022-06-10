b0c31822ab8a4b1420860dbc5fea7c1bebe02bdb
Marcus Rodrigues <markynrodrisilva@gmail.com>
2022-06-09 22:28:49 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+version="1.0"
+echo "# Script developed by mavinsi"
+echo "# github.com/mavinsi/gbman"
+echo "# gbman version $version"
+echo ""
+echo "- Installing..."
+echo "- Copying gbman to /bin/ folder"
+sudo cp gbman /usr/local/bin/gbman
+echo "- gbman $version was sucessfully installed!"
